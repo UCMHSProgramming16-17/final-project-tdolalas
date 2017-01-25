ac2752c2dda5427dc66ecadb2bed642022b0fc2f
Thomas Dolalas <tdolalas@ucvts.org>
2017-01-25 16:49:26 +0000

    Second chart

--- death2.py
+++ death2.py
@@ -4,4 +4,8 @@ df = pd.read_csv('death.csv')
 print(df)
 
 from bokeh.charts import Bar, output_file, save
-output_file('death.html')
+output_file('death2.html')
+
+sickdeath = Bar(df, 'Year', values='Pneumonia and Influenza Deaths', agg='mean', title= 'Average Pneumonia/Influenza deaths per year' )
+sickdeath.plot_width=1000
+save(sickdeath)
