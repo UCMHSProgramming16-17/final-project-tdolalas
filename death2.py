7266ca2457ca13e362e875ff97293436b2a99059
Thomas Dolalas <tdolalas@ucvts.org>
2017-01-25 18:00:15 +0000

    Second Chart color change

--- death2.py
+++ death2.py
@@ -6,6 +6,6 @@ print(df)
 from bokeh.charts import Bar, output_file, save
 output_file('death2.html')
 
-sickdeath = Bar(df, 'Year', values='Pneumonia and Influenza Deaths', agg='mean', title= 'Average Pneumonia/Influenza deaths per year' )
+sickdeath = Bar(df, 'Year', values='Pneumonia and Influenza Deaths', agg='mean', title= 'Average Pneumonia/Influenza deaths per year', color="ivory" )
 sickdeath.plot_width=1000
 save(sickdeath)
